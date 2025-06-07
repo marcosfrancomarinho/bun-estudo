c8830a00befb37f0cebce8494e5f53dbeb3714a1
marcosmarinho <marcosmarinho19998@gmail.com>
2025-06-07 18:00:29 -0300

    update

--- src/presentation/routers/Routers.ts
+++ src/presentation/routers/Routers.ts
@@ -8,7 +8,7 @@ export class Routers {
     const { creatorUserControllers, finderUserByNameControllers, finderUserAllControllers } = container.dependencies();
 
     this.server.on('post', '/create-user', creatorUserControllers.execute.bind(creatorUserControllers));
-    this.server.on('get', '/finder-user', finderUserByNameControllers.execute.bind(finderUserByNameControllers));
-    this.server.on('get', '/finder-user-all', finderUserAllControllers.execute.bind(finderUserAllControllers));
+    this.server.on('get', '/find-user', finderUserByNameControllers.execute.bind(finderUserByNameControllers));
+    this.server.on('get', '/find-user-all', finderUserAllControllers.execute.bind(finderUserAllControllers));
   }
 }
